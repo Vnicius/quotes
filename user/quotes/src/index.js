d1bca7043c0b8625f051d6df075ad0b2c0370262
Vinicius <vinicius.matheus252@gmail.com>
2018-01-10 15:59:00 -0300

    Add provider

--- user/quotes/src/index.js
+++ user/quotes/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import App from './containers/App';
 import Home from './containers/Home';
@@ -10,11 +12,13 @@ import TopQuotes from './containers/TopQuotes';
 ReactDOM.render(
     (
         <BrowserRouter>
-            <App>
-                <Route exact path='/' component={Home} />
-                <Route path='/form' component={FormQuote} />
-                <Route path='/top' component={TopQuotes} />
-            </App>
+            <Provider store={store}>
+                <App>
+                    <Route exact path='/' component={Home} />
+                    <Route path='/form' component={FormQuote} />
+                    <Route path='/top' component={TopQuotes} />
+                </App>
+            </Provider>
         </BrowserRouter>
     ),
     document.getElementById('root'));
