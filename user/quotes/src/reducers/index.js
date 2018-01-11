fcf4ec798887aa7eb2d2205cee36c42de686ea1d
Vinicius <vinicius.matheus252@gmail.com>
2018-01-11 13:20:19 -0300

    Form reducer

--- user/quotes/src/reducers/index.js
+++ user/quotes/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import top from './top-quotes-reducer';
 import random from './random-quotes-reducer';
+import form from './form-reducer';
 
 export default combineReducers({
     top: top,
-    random: random
+    random: random,
+    form: form
 });
