5e95481638e2c982e6e542af4ada3fadb63f9f42
Vinicius <vinicius.matheus252@gmail.com>
2018-01-12 19:45:45 -0300

    Handler to like button in the random container

--- user/quotes/src/actions/random-quotes-actions.js
+++ user/quotes/src/actions/random-quotes-actions.js
@@ -6,3 +6,15 @@ export function fetch(quant) {
     return {type: "FETCH_QUOTES",
             payload: axios.get('http://localhost:8888/api/quotes/radomquotes',{params:{quant : quant}})}
 }
+
+export function handlerLike(id) {
+    if(localStorage.getItem(id)){
+        return {
+            type:"PASS"
+        }
+    }
+    return {
+        type: "LIKE",
+        payload: axios.put('http://localhost:8888/api/quotes/like/', { id: id})
+    }
+}
